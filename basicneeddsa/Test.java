bfdebc47f61084dcc11cc70a04019ca22ea3f4c5
SubhamSekharNayak <dikibunty1810@gmail.com>
2026-04-27 21:07:32 +0530

    Added a single file

---
+++ basicneeddsa/Test.java
@@ -0,0 +1,48 @@
+// class A
+// {
+// 	static void show()
+// 	{
+// 		System.out.println("method show");
+// 	}
+// 	void look()
+// 	{
+// 		System.out.println("method look");
+// 	}
+// }
+// class Test
+// {
+// 	public static void main(String args[])
+// 	{
+// 		A.show();
+		
+
+// 		A obj=new A();
+// 		obj.look();
+// 		obj.show();
+// 	}
+// }
+
+
+class A
+{
+	int x=0, y=0;
+	static int z=0;
+}
+class Test
+{
+	public static void main(){
+		System.out.println(A.z);
+		// System.out.println(A.x);// error
+		A ob=new A();
+		A obj=new A();
+		ob.x=10;
+		obj.x=20;
+		ob.y=5;
+		obj.y=15;
+		System.out.println(ob.x +" "+ obj.y);
+		System.out.println(ob.y +" "+ obj.x);
+		ob.z=30;
+		obj.z=0; 
+		System.out.println(ob.z +" "+ obj.z +" "+ A.z);
+	}
+}
